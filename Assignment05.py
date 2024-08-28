6081a7a9bfdd7b5f86b66bddf86b06eed17ade1e
DL <176932570+dl-11@users.noreply.github.com>
2024-08-27 18:44:34 -0700

    Add files via upload
    Changed from lists to dictionaries, added error handling

--- Assignment05.py
+++ Assignment05.py
@@ -3,7 +3,6 @@
 # Desc: This assignment demonstrates using dictionaries, files, and exception handling
 # Change Log: (Who, When, What)
 #   David Levinson,8/24/2024,Created Script
-#   <Your Name Here>,<Date>, <Activity>
 # ------------------------------------------------------------------------------------------ #
 
 # Define the Data Constants
@@ -19,65 +18,96 @@ MENU: str = '''
 # Define the Data Constants
 FILE_NAME: str = "Enrollments.csv"
 
-# Define the Data Variables and constants
+# Define the Data Variables
 student_first_name: str = ''  # Holds the first name of a student entered by the user.
 student_last_name: str = ''  # Holds the last name of a student entered by the user.
 course_name: str = ''  # Holds the name of a course entered by the user.
-student_data: list = []  # one row of student data
+student_data: dict = {}  # one row of student data in dict form
 students: list = []  # a table of student data
 csv_data: str = ''  # Holds combined string data separated by a comma.
 file = None  # Holds a reference to an opened file.
 menu_choice: str  # Hold the choice made by the user.
 
-
 # When the program starts, read the file data into a list of lists (table)
-# Extract the data from the file
-file = open(FILE_NAME, "r")
-for row in file.readlines():
+# Extract the data from the file, with added exception handling
+try:
+    file = open(FILE_NAME, 'r')
     # Transform the data from the file
-    student_data = row.split(',')
-    student_data = [student_data[0], student_data[1], student_data[2].strip()]
-    # Load it into our collection (list of lists)
-    students.append(student_data)
-file.close()
-
+    for row in file.readlines():
+        parts = row.strip().split(",")
+        student_first_name = parts[0]
+        student_last_name = parts[1]
+        course = parts[2]
+        student_data = {'First_Name': student_first_name, 'Last_Name': student_last_name, 'Course': course}
+        students.append(student_data)
+except FileNotFoundError as errortext:
+    print("Text file must exist before running this script!\n")
+except Exception as errortext:
+    print("There was a non-specific error!\n")
+    print("Built-In Python error info: ")
+    print(errortext, errortext.__doc__, type(errortext), sep='\n')
+finally:
+    if file:
+        file.close()
 # Present and Process the data
-while (True):
-
+while True:
     # Present the menu of choices
     print(MENU)
     menu_choice = input("What would you like to do: ")
 
     # Input user data
     if menu_choice == "1":  # This will not work if it is an integer!
-        student_first_name = input("Enter the student's first name: ")
-        student_last_name = input("Enter the student's last name: ")
-        course_name = input("Please enter the name of the course: ")
-        student_data = [student_first_name,student_last_name,course_name]
+        while True:
+            try:
+                student_first_name: str = input("Enter the student's first name: ")
+                if not student_first_name.isalpha():
+                    raise ValueError("First name must contain only alphabetic characters.")
+                break
+            except ValueError as errortext:
+                print(errortext)
+        while True:
+            try:
+                student_last_name: str = input("Enter the student's last name: ")
+                if not student_last_name.isalpha():
+                    raise ValueError("Last name must contain only alphabetic characters.")
+                break
+            except ValueError as errortext:
+                print(errortext)
+
+        course: str = input("Please enter the name of the course: ")
+        student_data: dict = {'First_Name': student_first_name,
+                              'Last_Name': student_last_name,
+                              'Course': course}
         students.append(student_data)
-        print(f"You have registered {student_first_name} {student_last_name} for {course_name}.")
         continue
 
     # Present the current data
-    elif menu_choice == "2":
+    if menu_choice == "2":
 
         # Process the data to create and display a custom message
-        print("-"*50)
-        for student in students:
-            print(f"Student {student[0]} {student[1]} is enrolled in {student[2]}")
-        print("-"*50)
+        print("-" * 50)
+        for row in students:
+            print(f"{row['First_Name']} {row['Last_Name']} is registered for {row['Course']}")
+        print("-" * 50)
         continue
 
     # Save the data to a file
     elif menu_choice == "3":
-        file = open(FILE_NAME, "w")
-        for student in students:
-            csv_data = f"{student[0]},{student[1]},{student[2]}\n"
-            file.write(csv_data)
-        file.close()
-        print("The following data was saved to file!")
-        for student in students:
-            print(f"Student {student[0]} {student[1]} is enrolled in {student[2]}")
+        try:
+            file = open(FILE_NAME, "w")
+            for row in students:
+                csv_data = f'{row["First_Name"]},{row["Last_Name"]},{row["Course"]}\n'
+                file.write(csv_data)
+            file.close()
+        except Exception as e:
+            print(e)
+            print("The following data was saved to file!")
+            for row in students:
+                print(f"{row['First_Name']} {row['Last_Name']} is registered for {row['Course']}")
+        except  TypeError as errortext:
+            print(errortext)
+        except Exception as errortext:
+            print(errortext)
         continue
 
     # Stop the loop
